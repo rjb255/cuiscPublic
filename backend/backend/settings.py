cff9a21f586abfc284019f42277a1cecb3ad39d7
rjb255 <rjb255@cam.ac.uk>
2021-10-31 19:32:53 +0000

    fair few changess

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import path
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {"title": "Home"}
+
+    return render(request, 'index.html', context)
