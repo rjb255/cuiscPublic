a96d7046906b5d39f001e01b3b3732275a384395
Ross Brown <rjb255@cam.ac.uk>
2021-11-14 20:13:03 +0000

    removed django (overkill at this point)

--- backend/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from os import path
-
-
-def index(request):
-    context = {"title": "Home"}
-
-    return render(request, 'index.html', context)
